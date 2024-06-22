8b00877f97bd5c937226ff25d7994da3457f0f78
ZengFLab <90915859+ZengFLab@users.noreply.github.com>
2024-06-22 21:33:02 +0800

    create repo

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # SURE_example_2
  
+In this tutorial, we demonstrate the process of using SURE for hierarchical assembly. We continue to use the competition data provided by NeurIPS 2021 as a demonstration example. This data contains two types of batch information: one is the location where the data was generated, and the other is the sample. Considering both the location and sample batch factors, there are a total of 13 batches, some of which have too few cells. To ensure that each batch has enough cells for computation, we treat the single-cell data generated at each location as an atlas and use SURE to establish local coordinate systems. Then, we assemble the local coordinate systems to form a global coordinate system.
+
+In this example, we provide the following code covering:
+1. [Preparing the data for each location to establish local coordinate systems](./batch_prepare_mtx_files_4_SURE.py)
+2. Computing the primary metacells for each local coordinate system
+3. Preparing the data for establishing the global coordinate system
+4. Computing the secondary metacells for the global coordinate system
+5. Visualizing the results
---
+++ batch_prepare_mtx_files_4_SURE.py
@@ -0,0 +1,57 @@
+import pandas as pd
+import numpy as np
+import scanpy as sc
+import subprocess
+
+import datatable as dt
+
+from sklearn.preprocessing import OneHotEncoder
+
+batch_size = 10000 # expected number of cells in each partitioned file
+n_top_genes = 3000 # number of HVGs being used
+target_sum = 1e4 # use for finding HVGs
+
+cmd='rm *_B*_*.gz'
+subprocess.call(cmd, shell=True)
+
+print('load h5ad')
+adata_ = sc.read_h5ad('GSE194122_openproblems_neurips2021_multiome_BMMC_processed.h5ad')
+adata_.layers['counts'] = adata_.X.copy()
+
+adata_.obs['Batch'] = adata_.obs['Site']
+batches = adata_.obs['Batch'].value_counts().reset_index()
+batches.columns = ['Batch','Value']
+
+parts = []
+conds = np.unique(batches['Batch'].tolist())
+for co in conds: 
+    tmp = adata_[adata_.obs.Batch.isin([co])].copy()
+    total_size = tmp.shape[0]
+    num_batches = int(np.ceil(total_size / batch_size))
+    num_batches = num_batches if num_batches > 0 else 1
+    pas = np.array_split(tmp.obs_names.tolist(), num_batches)
+    parts.extend(pas)
+
+
+parts_ = parts
+parts = [x for x in parts_ if len(x)>0 ]
+
+
+for i in np.arange(len(parts)):
+    cells = parts[i]
+    adata = adata_[cells].copy()
+    batch='B{}'.format(i+1)
+
+    print(f'{batch} /{len(parts)}    size ' + '{}'.format(adata.shape))
+    
+    sc.pp.normalize_total(adata, target_sum=target_sum)
+    sc.pp.log1p(adata)
+    sc.pp.highly_variable_genes(adata, n_top_genes=n_top_genes, subset=True)
+
+    dt.Frame(pd.DataFrame(adata.layers['counts'].toarray(), columns=adata.var_names, dtype=np.float64)).to_csv(f'multiome_neurips21_{batch}_counts.txt.gz')
+
+    adata.obs_names.to_frame().to_csv(f'multiome_neurips21_{batch}_cell.csv.gz', index=None)
+    adata.var_names.to_frame().to_csv(f'multiome_neurips21_{batch}_gene.csv.gz', index=None)
+
+    enc = OneHotEncoder(sparse_output=False).fit(adata.obs['Batch'].to_numpy().reshape(-1,1))
+    pd.DataFrame(enc.transform(adata.obs['Batch'].to_numpy().reshape(-1,1)), columns=enc.categories_).to_csv(f'multiome_neurips21_{batch}_uwv.txt.gz', index=False)
